eca71746dc421b07c45161d9c8f9427a437cd2cc
Durval Printes <durvalprintes@gmail.com>
2019-07-27 12:15:14 -0300

    Continuando a refatoração

--- desafio.js
+++ desafio.js
@@ -1,15 +1,15 @@
-import { get, post } from 'axios';
-import { resolve } from 'path';
-import { writeFileSync, createReadStream } from 'fs';
-import sha1 from 'sha1';
-import Form from 'form-data';
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+const sha1 = require('sha1');
+const Form = require('form-data');
 
 async function score() {
   try {
     const token = 'd55d68f8cce28a574a29f9c58a25eeee3d849b1f';
-    const { data } = await get(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`);
-    const file = resolve(__dirname, 'answer.json');
-    writeFileSync(file, JSON.stringify(data));
+    const { data } = await axios.get(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`);
+    const file = path.resolve(__dirname, 'answer.json');
+    fs.writeFileSync(file, JSON.stringify(data));
     const { numero_casas } = data;
     let cifrado = data.cifrado.toLowerCase().split('');
     let decifrado = cifrado.map(letra => {
@@ -20,12 +20,12 @@ async function score() {
     });
     data.decifrado = decifrado.join('');
     data.resumo_criptografico = sha1(data.decifrado);
-    writeFileSync(file, JSON.stringify(data));
+    fs.writeFileSync(file, JSON.stringify(data));
     let form = new Form();
-    form.append('answer', createReadStream(file, 'utf8'));
+    form.append('answer', fs.createReadStream(file, 'utf8'));
     const url = `https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=${token}`;
-    const config = { headers: form.getHeaders() }
-    const score = await post(url, form, config);
+    const config = { headers: form.getHeaders() };
+    const score  = await axios.post(url, form, config);
     console.log(score.data);
   } catch (error) {
     console.error(error);
